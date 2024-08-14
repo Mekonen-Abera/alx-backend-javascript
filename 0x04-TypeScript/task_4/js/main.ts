5eea6763ff20079ed06e5b6394e9bc32a8623e7e
Mekonen-Abera <makoninabera@gmail.com>
2024-08-14 07:59:08 +0000

    Typescript

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,26 @@
+namespace Subjects {
+    export const cpp = Cpp;
+    export const java = Java;
+    export const react = React;
+    // @ts-ignore
+    export const cTeacher: Teacher
+    {
+        "test";
+        "test";
+        10
+    }
+    /*cpp.teacher = cTeacher;
+    console.log(cpp.name);
+    console.log(cpp.getRequirements());
+    console.log(cpp.getAvailableTeacher());
+    java.teacher = cTeacher;
+    console.log(java.name);
+    console.log(java.getRequirements());
+    console.log(java.getAvailableTeacher());
+    react.teacher = cTeacher;
+    console.log(react.name);
+    console.log(react.getRequirements());
+    console.log(react.getAvailableTeacher());*/
+
+
+}
